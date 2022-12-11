a414c020fd28d5a5a49d09d8322c4cd803d4e1ef
Valentin Manceaux-Panot <va.mcxp@gmail.com>
2022-12-11 16:44:38 +0100

    Added a media card for better sharing

---
+++ pages/github/[user]/[year].tsx
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const Preview = () => {
+  const router = useRouter();
+
+  const { user, year } = router.query;
+
+  router.push("/");
+
+  return (
+    <Head>
+      <title>
+        {user}'s {year} open source contributions
+      </title>
+      <meta
+        property="og:title"
+        content={`${user}'s ${year} open source contributions`}
+        key="title"
+      />
+      <meta
+        property="og:image"
+        content={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_NAME}/image/upload/${user}-${year}.svg`}
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
+  );
+};
+
+export default Preview;
