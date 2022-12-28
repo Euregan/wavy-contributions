1024d2f6267f35b3ab767a9f70f104d3591ef1c5
Valentin Manceaux-Panot <va.mcxp@gmail.com>
2022-12-28 09:57:04 +0100

    Update TwitterShare.tsx

--- ui/TwitterShare.tsx
+++ ui/TwitterShare.tsx
@@ -11,7 +11,7 @@ const TwitterShare = ({ user, year }: Props) => (
     href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
       `Here's my ${year} commits!`
     )}&url=${encodeURIComponent(
-      `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_VERCEL_URL}/github/${user}/${year}`
+      `${process.env.NEXT_PUBLIC_PROTOCOL}://${process.env.NEXT_PUBLIC_VERCEL_URL}/${user}/${year}`
     )}`}
     target="_blank"
   >
